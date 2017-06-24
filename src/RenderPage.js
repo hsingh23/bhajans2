7706ab855d8aefac07d86898a8df94770523a345
Harsh Singh <harsingh@cimpress.com>
2017-06-23 23:40:01 -0500

    try

--- src/RenderPage.js
+++ src/RenderPage.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const canRenderPdfNatively = function() {
+  function hasAcrobatInstalled() {
+    function getActiveXObject(name) {
+      try {
+        return new ActiveXObject(name); // eslint-disable-line
+      } catch (e) {}
+    }
+
+    return getActiveXObject('AcroPDF.PDF') || getActiveXObject('PDF.PdfCtrl');
+  }
+
+  function isIos() {
+    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  }
+
+  return navigator.mimeTypes['application/pdf'] || hasAcrobatInstalled() || isIos();
+};
+
 const RenderPage = props => {
   const [book, page] = props.match.params.location.split('-');
   return (
@@ -13,7 +31,7 @@ const RenderPage = props => {
         </nav>
       </div>
       <div className="rest">
-        <embed src={`/pdfs/${book}.pdf#page=${page}`} style={{ width: '100vw', height: 'calc( 100vh - 56px )' }} />
+       { canRenderPdfNatively() ? <embed src={`/pdfs/${book}.pdf#page=${page}`} style={{ width: '100vw', height: 'calc( 100vh - 56px )' }} /> : <iframe src={`https://docs.google.com/gview?embedded=true&url=https://${window.location.host}/pdfs/${book}.pdf#page=${page}`} frameborder="0" style={{ width: '100vw', height: 'calc( 100vh - 56px )' }} ></iframe> }
       </div>
     </div>
   );
