d18020a694bd5baca01f1027064de7efefed7f11
Harsh Singh <harsingh@cimpress.com>
2017-06-25 15:18:45 -0400

    minify pdfs and update service worker

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   stripPrefix: 'build/',
-  staticFileGlobs: ['build/*.html', 'build/static/**/!(*map*)', 'build/pdfs/*.pdf', 'bhajan-index.json', 'build/*.png', 'build/*.jpg', 'build/*.ico'],
+  staticFileGlobs: ['build/*.html', 'build/static/**/!(*map*)', 'build/pdfs/*.pdf', 'build/bhajan-index.json', 'build/*.png', 'build/*.jpg', 'build/*.ico'],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
   swFilePath: 'build/service-worker.js',
 };
