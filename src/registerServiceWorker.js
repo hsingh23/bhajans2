b4342a9154cfde41c969936c1745d5ec2a01a42d
Harsh Singh <harsingh@cimpress.com>
2017-07-08 02:32:55 -0500

    css fixup

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -11,7 +11,7 @@
 import { alert } from 'notie';
 const prod = process.env.NODE_ENV === 'production';
 export default function register() {
-  if (true && 'serviceWorker' in navigator) {
+  if (prod && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       navigator.serviceWorker
