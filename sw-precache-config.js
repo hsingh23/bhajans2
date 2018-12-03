d94c7fa70b0ac2771b8fd53566001cc5202b0b5e
Harsh Singh 🔥 💯 🦄 <harsh.singh@tamr.com>
2018-12-02 23:02:48 -0500

    fix: serve service worker from both old and new URLs
    Copy build/service-worker2.js to build/service-worker.js in the
    build script so clients requesting either URL get a valid
    worker. Also add the 512x512 android-chrome icon referenced by
    the manifest.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  stripPrefix: "build/",
+  stripPrefix: 'build/',
   maximumFileSizeToCacheInBytes: 4097152,
   staticFileGlobs: [
-    "build/*.html",
-    "build/static/**/!(*map*)",
-    "build/bhajan-index2.json",
-    "build/*.min.css",
-    "build/*.min.js",
-    "build/*.png",
-    "build/*.jpg",
-    "build/*.ico",
-    "build/pdfs/*.pdf"
+    'build/*.html',
+    'build/static/**/!(*map*)',
+    'build/bhajan-index2.json',
+    'build/*.min.css',
+    'build/*.min.js',
+    'build/*.png',
+    'build/*.jpg',
+    'build/*.ico',
+    'build/pdfs/*.pdf',
   ],
-  navigateFallback: "build/index.html",
+  navigateFallback: 'build/index.html',
   ignoreUrlParametersMatching: [/./],
-  importScripts: ["firebase-messaging-sw.js"],
+  importScripts: ['firebase-messaging-sw.js'],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
-  swFilePath: "build/service-worker2.js",
+  swFilePath: 'build/service-worker2.js',
   runtimeCaching: [
     {
       urlPattern: /cdn.polyfill.io.v2.polyfill.min.js/,
-      handler: "fastest"
-    }
-  ]
+      handler: 'fastest',
+    },
+  ],
 };
