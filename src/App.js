c6c9f1fc9c20b6233dd67e27487c858056973cc4
Harsh Singh <hisingh1@gmail.com>
2019-05-13 22:12:40 -0400

    Upgrade deps (#9)
    * safe updates
    
    * adds info component and ability to stop audio
    
    * update index - the old one has many repeats

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { Component } from 'react';
-import { getJson, setJson, PropsRoute } from './util';
-import { whenUser, setRefOnce, removeRefOnce, checkRefOnce, auth } from './firebase';
-import { omit, get, orderBy } from 'lodash-es';
+import React, { Component } from "react";
+import { getJson, setJson, PropsRoute } from "./util";
+import { whenUser, setRefOnce, removeRefOnce, checkRefOnce, auth } from "./firebase";
+import { omit, get, orderBy } from "lodash-es";
 
 // import { confirm } from "notie";
-import { Redirect, Switch } from 'react-router-dom';
-import Search from './Search';
-import RenderPage from './RenderPage';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHeart, faMusic, faPlay, faStop, faCompactDisc, faCartArrowDown } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Redirect, Switch } from "react-router-dom";
+import Search from "./Search";
+import RenderPage from "./RenderPage";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faHeart,
+  faMusic,
+  faPlay,
+  faStop,
+  faCompactDisc,
+  faCartArrowDown,
+  faInfo
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-library.add(faHeart, faMusic, faPlay, faStop, faCompactDisc, faCartArrowDown);
+library.add(faHeart, faMusic, faPlay, faStop, faCompactDisc, faCartArrowDown, faInfo);
 class App extends Component {
   constructor(props) {
     super(props);
     // You must be logged in to call firebase and get your favorites (rule) and also set your favorites
     // if you don't care about syncing between devices, we can use localstorage for favorites
-    const favorites = getJson('favorites') || {};
-    setJson('favorites', favorites);
+    const favorites = getJson("favorites") || {};
+    setJson("favorites", favorites);
     this.state = { favorites, bhajans: [] };
   }
 
@@ -29,17 +37,17 @@ class App extends Component {
           checkRefOnce(`favorites/${user.uid}`).then(favorites => {
             favorites = Object.assign({}, self.state.favorites, favorites);
             self.setState({ favorites });
-            setJson('favorites', favorites);
+            setJson("favorites", favorites);
           });
-        },
+        }
       };
     })(this);
 
     window
-      .fetch('/bhajan-index2.json')
+      .fetch("/bhajan-index2.json")
       .then(data => data.json())
       .then(fetchedBhajans => {
-        fetchedBhajans = orderBy(fetchedBhajans, ['n', 't'], ['asc', 'asc']);
+        fetchedBhajans = orderBy(fetchedBhajans, ["n", "t"], ["asc", "asc"]);
         window.fetchedBhajans = fetchedBhajans;
         this.setState({ bhajans: fetchedBhajans });
       });
@@ -53,9 +61,9 @@ class App extends Component {
     // this.addFavorite[name] = 1;
     const favorites = Object.assign({ [name]: 1 }, this.state.favorites);
     this.setState({ favorites });
-    setJson('favorites', favorites);
-    const uid = get(auth, 'currentUser.uid');
-    uid && setRefOnce(`favorites/${auth.currentUser.uid}/${name}`, '1');
+    setJson("favorites", favorites);
+    const uid = get(auth, "currentUser.uid");
+    uid && setRefOnce(`favorites/${auth.currentUser.uid}/${name}`, "1");
   };
 
   removeFavorite = name => {
@@ -63,24 +71,26 @@ class App extends Component {
     // this.removeFavorite[name] = 1;
     const favorites = omit(this.state.favorites, name);
     this.setState({ favorites }, () => {
-      window.location.hash.includes('/my-favorites');
+      window.location.hash.includes("/my-favorites");
     });
-    setJson('favorites', favorites);
-    const uid = get(auth, 'currentUser.uid');
+    setJson("favorites", favorites);
+    const uid = get(auth, "currentUser.uid");
     uid && removeRefOnce(`favorites/${auth.currentUser.uid}/${name}`);
   };
 
   renderFavorite = (name, activeClassName, inactiveClassName) => {
     return this.state.favorites[name] ? (
       <button
-        className={activeClassName || 'button button-3d button-caution button-circle button-jumbo'}
-        onClick={() => this.removeFavorite(name)}>
+        className={activeClassName || "button button-3d button-caution button-circle button-jumbo"}
+        onClick={() => this.removeFavorite(name)}
+      >
         <FontAwesomeIcon icon="heart" color="white" />
       </button>
     ) : (
       <button
-        className={inactiveClassName || 'button button-3d button-circle button-jumbo'}
-        onClick={() => this.addFavorite(name)}>
+        className={inactiveClassName || "button button-3d button-circle button-jumbo"}
+        onClick={() => this.addFavorite(name)}
+      >
         <FontAwesomeIcon icon="heart" color="grey" />
       </button>
     );
