40cd2e1f41d2b0f76ac3ba54b1d2e153b330c3b7
Harsh Singh ��💯🐐 <hsingh23@illinois.edu>
2023-01-07 19:18:04 +0530

    chore: fix admin find user

--- src/Plans.js
+++ src/Plans.js
@@ -3,19 +3,19 @@ export const PLANS = [
     value: "oneIndividual10",
     label: "1 Year - $9.99",
     price: 9.99,
-    time: 31536000000
+    time: 31536000000,
   },
   {
     value: "tenIndividual50",
-    label: "10 Year - 49.99",
+    label: "10 Year - $49.99",
     price: 49.99,
     time: 315360000000,
-    isBest: true
+    isBest: true,
   },
   {
     value: "fiveIndividual40",
     label: "5 Year - $39.99",
     price: 39.99,
-    time: 157680000000
-  }
+    time: 157680000000,
+  },
 ];
